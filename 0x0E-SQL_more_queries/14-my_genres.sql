0a5af1eef1dad1eb7cbed67d0a3c94fb3220ca5f
Kebaso48 <kebasovictor9@gmail.com>
2023-05-18 08:33:06 -0700

    show by genre

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Lists all genres of the show Dexter in the database hbtn_0d_tvshows.
+-- Records are ordered by ascending genre name.
+SELECT tv_genres.name
+  FROM tv_shows
+  INNER JOIN tv_show_genres
+  ON tv_shows.id = tv_show_genres.show_id
+  INNER JOIN tv_genres
+  ON tv_show_genres.genre_id = tv_genres.id
+  WHERE tv_shows.title = "Dexter"
