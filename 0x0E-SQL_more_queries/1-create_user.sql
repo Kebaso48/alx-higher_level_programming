1abcb0b025338038497743b13cc67fea8310670e
Kebaso48 <kebasovictor9@gmail.com>
2023-05-18 05:16:25 -0700

    states

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,8 @@
--- ceates user user_0d_1with all priviledges
+-- creates user user_0d_1with all priviledges
 CREATE USER
-	IF NOT EXISTS 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd'
+IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-	ON *.*
-	TO 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES
+ON *.*
+TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
